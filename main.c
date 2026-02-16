90aaaf232b2a9ff09da0115c5ed92bfa9a225b9d
Beginner10617 <wasihusain23@iitk.ac.in>
2026-02-16 12:08:03 +0530

    reading labels

--- main.c
+++ main.c
@@ -16,9 +16,9 @@ void swapInPlace(uint32_t* arr, int size){
 	return;
 }
 
-void readHeader(uint32_t* head, FILE* ptr){
+void readImgHeader(uint32_t* head, FILE* ptr){
 	if(fread(head, 4, 4, ptr) != 4){
-		printf("Error reading the header\n");
+		printf("Error reading the Image header\n");
 		fclose(ptr); exit(1);	
 	}
 	swapInPlace(head, 4);
@@ -28,6 +28,19 @@ void readHeader(uint32_t* head, FILE* ptr){
 	printf("number of rows: %d\n", head[2]);
 	printf("number of columns: %d\n", head[3]);
 	printf("-------------------------\n"); 
+	return;
+}
+
+void readLabelHeader(uint32_t* head, FILE* ptr){
+	if(fread(head, 4, 2, ptr) != 2){
+		printf("Error reading the Label header\n");
+		fclose(ptr); exit(1);
+	}
+	swapInPlace(head, 2);
+	printf("-------------------------\n");
+	printf("magic number: %d\n", head[0]);
+	printf("number of images: %d\n", head[1]);
+	printf("-------------------------\n");
 	return;
 }
 
@@ -42,22 +55,43 @@ int readNextImage(uint8_t* img, int img_size, FILE* ptr){
 }
 
 int main(){
-	FILE *fptr;
-	fptr = fopen("dataset/train-images-idx3-ubyte", "rb");
-	if (fptr == NULL) {
+	FILE *fptrimg, *fptrlabel;
+	
+	fptrimg = fopen("dataset/train-images-idx3-ubyte", "rb");
+	fptrlabel = fopen("dataset/train-labels-idx1-ubyte","rb");
+	if (fptrimg == NULL || fptrlabel == NULL) {
     		printf("Error opening file!\n");
 		return 1;
 	}
-	uint32_t header[4];
-	readHeader(header, fptr);
-	int image_size = header[2] * header[3];
+	uint32_t imgheader[4], labelheader[2];
+	readImgHeader(imgheader, fptrimg);
+	readLabelHeader(labelheader, fptrlabel);
+	int image_size = imgheader[2] * imgheader[3];
+	uint8_t label = 0;
 	uint8_t image[image_size];
-	int x=0;
-	while(readNextImage(image, image_size, fptr)){
+	int x=0, y=0;
+	while(readNextImage(image, image_size, fptrimg)){
+		if(fread(&label, 1, 1, fptrlabel) != 1){
+			printf("Error reading label\n");
+			break;
+		}
+		for(int a=0; a<imgheader[2]; a++){
+			for(int b=0; b<imgheader[3]; b++){
+				if(image[b+a*imgheader[2]]>0)
+					printf("@");
+				else
+					printf(" ");
+			}
+			printf("\n");
+		}
+		printf("%u\n", label);
+		scanf("%d", &y);
+		if(y==0) break;
 		x++;
-		if(x >= header[1]) break;
+		if(x >= imgheader[1]) break;
 	}
 	printf("%d images read succedfully\n", x);
-    	fclose(fptr);
+    	fclose(fptrimg);
+	fclose(fptrlabel);
 	return 0;
 }
