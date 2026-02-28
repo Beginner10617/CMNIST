3bc77ee4c6605003a969587cac5d21f7bb347611
Beginner10617 <wasihusain23@iitk.ac.in>
2026-03-01 02:45:04 +0530

    added comments to fileSystem.h header file

--- fileSystem.h
+++ fileSystem.h
@@ -3,7 +3,10 @@
 #include "stdio.h"
 #include "neuron.h"
 
+// Save MLP parameters in a textfile
 void saveMLP(MLP* mlp, char* Fname);
+
+// Load MLP from parameters specified in a textfile
 MLP* loadMLP(char* Fname);
 
 #endif
